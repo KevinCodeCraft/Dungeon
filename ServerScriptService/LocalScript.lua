c3d30e45ed45453d326fd28727c8616f84a8e8fa
KevinCodeCraft <161757318+KevinCodeCraft@users.noreply.github.com>
2024-07-29 16:17:36 +0200

    Update

--- ServerScriptService/LocalScript.lua
+++ ServerScriptService/LocalScript.lua
@@ -1,13 +1,8 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage");
-local TPRemote = ReplicatedStorage:WaitForChild("TeleportRemote");
+local Player = game.Players.LocalPlayer;
+local Leaderstats = Player:WaitForChild("leaderstats");
 
-local Debounce = os.clock() - 30;
+script.Parent.Text = `${Leaderstats.Coins.Value}`
 
-script.Parent.Activated:Connect(function()
-	if os.clock() - Debounce < 30 then
-		return	
-	end
-	
-	Debounce = os.clock();
-	TPRemote:FireServer(18452655500);
+Leaderstats.Coins.Changed:Connect(function()
+	script.Parent.Text = `${Leaderstats.Coins.Value}`
 end)
