a37d0457728b310161ff91dbcf4ba578914da64e
KevinCodeCraft <161757318+KevinCodeCraft@users.noreply.github.com>
2024-07-29 16:20:33 +0200

    Update

--- StarterGui/LocalScript.lua
+++ StarterGui/LocalScript.lua
@@ -1,9 +1,21 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
-local SwordEvent = ReplicatedStorage:WaitForChild("SwordEvent");
+local Remote = ReplicatedStorage:WaitForChild("CoinPopupEvent");
 
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false);
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false);
+local Sample = script.Sample;
 
-script.Parent.Activated:Connect(function()
-	SwordEvent:FireServer("StateChange");
+Remote.OnClientEvent:Connect(function(Amount : number)
+	Amount = tostring(Amount);
+	
+	local Popup = Sample:Clone();
+	Popup.Parent = script.Parent;
+	
+	Popup.TextLabel.Text = `+${Amount}`
+	
+	Popup.Position = UDim2.new(math.random(), 0, math.random(), 0);
+	
+	local Time = 2 * math.abs(Popup.Position.Y.Scale, -1.2);
+	
+	Popup:TweenPosition(
+		UDim2.new(Popup.Position.X.Scale, 0, Popup.Position.Y.Scale - 1.2, 0), Enum.EasingDirection.In, Enum.EasingStyle.Quad, Time
+	);
 end)
