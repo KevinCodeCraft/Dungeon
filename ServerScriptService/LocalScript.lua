adc65bd986673c662b0b0a4110fa82d828242db6
KevinCodeCraft <161757318+KevinCodeCraft@users.noreply.github.com>
2024-07-29 16:17:38 +0200

    Update

--- ServerScriptService/LocalScript.lua
+++ ServerScriptService/LocalScript.lua
@@ -1,13 +1,4 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage");
-local TPRemote = ReplicatedStorage:WaitForChild("TeleportRemote");
+local Player = game.Players.LocalPlayer;
+local FastestTime = Player:WaitForChild("FastestTime");
 
-local Debounce = os.clock() - 30;
-
-script.Parent.Activated:Connect(function()
-	if os.clock() - Debounce < 30 then
-		return	
-	end
-	
-	Debounce = os.clock();
-	TPRemote:FireServer(18536859031, true);
-end)
+script.Parent.Text = `Fastest time: {FastestTime.Value}s`;
