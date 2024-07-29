e40c68e77150998061e0ab087989c8672ba57ff5
KevinCodeCraft <161757318+KevinCodeCraft@users.noreply.github.com>
2024-07-29 16:20:35 +0200

    Update

--- StarterGui/LocalScript.lua
+++ StarterGui/LocalScript.lua
@@ -1,31 +1,13 @@
-local RunService = game:GetService("RunService");
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local TPRemote = ReplicatedStorage:WaitForChild("TeleportRemote");
 
-script.Parent.Parent:GetPropertyChangedSignal("Enabled"):Wait();
+local Debounce = os.clock() - 30;
 
-local Time = 0;
-
-local function SecondsToMinutes()
-	local Seconds = math.round(Time % 60);
-	local Minutes = Time // 60;
-	
-	if Seconds < 10 then
-		Seconds = `0{Seconds}`;
-	else
-		Seconds = tostring(Seconds);
+script.Parent.Activated:Connect(function()
+	if os.clock() - Debounce < 30 then
+		return	
 	end
 	
-	if Minutes < 10 then
-		Minutes = `0{Minutes}`;
-	else
-		Minutes = tostring(Minutes);
-	end
-	
-	script.Parent.Text = `{Minutes}:{Seconds}`;
-end
-
-local function OnHeartBeat(DeltaTime)
-	Time = Time + DeltaTime;
-	SecondsToMinutes()
-end
-
-RunService.Heartbeat:Connect(OnHeartBeat) 
+	Debounce = os.clock();
+	TPRemote:FireServer(18452655500);
+end)
